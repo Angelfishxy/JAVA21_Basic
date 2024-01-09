4369a7479f3eb73c5538063cdcaba6ccca89e21b
Xinyu <xinyuzhao1222@gmail.com>
2024-01-09 10:41:07 +0800

    init project

--- day03/src/com/itheima/ternaryoperator/TernaryoperatorDemo.java
+++ day03/src/com/itheima/ternaryoperator/TernaryoperatorDemo.java
@@ -4,7 +4,9 @@ public class TernaryoperatorDemo {
     public static void main(String[] args) {
         //使用三元运算符获取两个数的最大值
         int num1 = 10;
-        int num2 = 20
+        int num2 = 20;
 
+        int max = num1 > num2 ? num1 :num2;
+        System.out.println (max);
     }
 }
