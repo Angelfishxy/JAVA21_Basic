7e4021d89148c2bd85dc3243c903feee68db1fe7
Xinyu <xinyuzhao1222@gmail.com>
2024-01-18 19:45:22 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/modules.xml

---
+++ day06/src/com/itheima/methoddemo/MethodDemo7.java
@@ -0,0 +1,13 @@
+package src.com.itheima.methoddemo;
+
+public class MethodDemo7 {
+    public static void main(String[] args) {
+        //方法的重载
+    }
+    public static int sum(int a ,int b){
+        return a + b;
+    }
+    public  static int sum(int a, int b, int c){
+        return a + b + c;
+    }
+}
