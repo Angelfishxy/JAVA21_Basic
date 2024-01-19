0653ebf9e915ebdd020ffd7f82873ff4465b4a26
Xinyu <xinyuzhao1222@gmail.com>
2024-01-19 20:33:00 +0800

    init

---
+++ day06/src/com/itheima/test/Test2.java
@@ -0,0 +1,13 @@
+package src.com.itheima.test;
+
+public class Test2 {
+    public static void main(String[] args) {
+        //遍历数组方法
+        int[] arr = {11, 22, 33, 44, 55};
+
+    }
+
+    //定义方法用于数组的遍历
+
+
+}
