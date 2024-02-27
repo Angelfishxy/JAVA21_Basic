ebcf26cceaa68dd108959ad0407072670c4c947e
Xinyu <xinyuzhao1222@gmail.com>
2024-02-27 17:13:03 +0800

    init

--- day06/src/com/itheima/test/Test6.java
+++ day06/src/com/itheima/test/Test6.java
@@ -4,7 +4,7 @@ public class Test6 {
     public static void main(String[] args) {
         //copy array
         int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] temp = copyOfRange (arr,3, 6);
+        int[] temp = copyOfRange (arr,3, 8);
         for (int i = 0; i < temp.length; i++) {
             System.out.print (temp[i] + " ");
         }
